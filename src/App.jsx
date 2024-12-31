1de5597494b2a972c029c03698c0d8a806cb4d63
preet <52096234+preet-pdf@users.noreply.github.com>
2025-01-01 01:21:50 +0530

    settingup my project

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import './App.css'
+import './index.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <h1 className="text-3xl font-bold underline p-3 m-3 text-gray-600" >Namste Everyone</h1>
+    </>
+  )
+}
+
+export default App
